0c0b82d06c2d26a1e6f256062c841489f3833d2c
JOEL_ALBERTO_PEREA_CASTORENA <a.22308051281272@cbtis128.edu.mx>
2025-02-26 20:45:05 +0000

    xd

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,166 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Joel Perea"),
+          titleTextStyle:
+              const TextStyle(color: Color(0xff1a01ff), fontSize: 20),
+          centerTitle: true,
+          backgroundColor: const Color(0xfff4b14d),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Joel Alberto Perea Castorena              "
+                "Mat: 22308051281272",
+                style: TextStyle(fontSize: 20, color: Color(0xffe86c07)),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              // Primera fila de iconos
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconWithLabel(icon: Icons.mobile_friendly, label: 'Movil'),
+                  IconWithLabel2(
+                      icon: Icons.dangerous_outlined, label: 'Peligro'),
+                  IconWithLabel3(icon: Icons.ice_skating, label: 'Patines'),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila de iconos
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconWithLabel4(icon: Icons.radar, label: 'Radar'),
+                  IconWithLabel5(icon: Icons.upgrade, label: 'Descargar'),
+                  IconWithLabel6(icon: Icons.mail, label: 'Gmail'),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+} // Fin Clase
+
+class IconWithLabel extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconWithLabel({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: Color(0xffe30909)),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class IconWithLabel2 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconWithLabel2({required this.icon, required this.label});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: Color(0xff72e309)),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+}
+
+class IconWithLabel3 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconWithLabel3({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: Color(0xff2a09e3)),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+}
+
+class IconWithLabel4 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconWithLabel4({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: Color(0xff09dce3)),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+}
+
+class IconWithLabel5 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconWithLabel5({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: Color(0xffe30990)),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+}
+
+class IconWithLabel6 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconWithLabel6({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: Color(0xff000000)),
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
